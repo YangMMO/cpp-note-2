b3d27eb16674b0f9165d3477313918726aed853d
YangMMO <11436052@qq.com>
2022-07-01 11:11:39 +0800

    push

--- 通讯录管理系统.cpp
+++ 通讯录管理系统.cpp
@@ -121,7 +121,139 @@ int isExist(AddressBooks *abs, string name)
     return -1;   // 不存在返回-1  
 }
 
+// 删除联系人
+void deletePerson(AddressBooks *abs)
+{
+    cout << "请输入要删除的联系人姓名：" << endl;
+    string name;
+    cin >> name;
 
+    int ret = isExist(abs, name);
+    if (ret != -1) {    // 存在
+        // 让被删除数据的后移一位往后数据前移一位
+        for (int i = 0; i < abs->m_Size; i++)
+        {
+            // 数据前移操作
+            abs->personArray[i] = abs->personArray[i + 1];
+        }
+
+        abs->m_Size--;  // 更新通讯录人数
+
+        cout << "删除成功" << endl;
+    } else {    // 不存在
+        cout << "没有找到该联系人" << endl;
+    }
+
+    system("pause");
+    system("cls");  //清屏
+    
+}
+
+// 查找联系人
+void findPseson(AddressBooks *abs)
+{
+    cout << "请输入要查找的联系人姓名：" << endl;
+    string name;
+    cin >> name;
+
+    int ret = isExist(abs, name);
+
+    if (ret != -1)
+    {
+        cout << "姓名：" << abs->personArray[ret].m_Name << "\t";
+        cout << "性别：" << (abs->personArray[ret].m_Gender == 1 ? "男" : "女") << "\t";
+        cout << "年龄：" << abs->personArray[ret].m_Age << "\t";
+        cout << "电话：" << abs->personArray[ret].m_Phone << "\t";
+        cout << "地址：" << abs->personArray[ret].m_Address << endl;
+    } else {
+        cout << "没有找到该联系人" << endl;
+    }
+    
+    system("pause");
+    system("cls");  //清屏
+
+}
+
+// 修改联系人
+void modifyPerson(AddressBooks *abs)
+{
+    cout << "请输入要修改的联系人姓名：" << endl;
+    string name;
+    cin >> name;
+
+    int ret = isExist(abs, name);
+    if (ret != -1) {    // 存在
+        // 输入姓名
+        string name;
+        cout << "请输入姓名：" << endl;
+        cin >> name;
+        abs->personArray[ret].m_Name = name;
+
+        // 输入性别
+        int gender = 0;
+        cout << "请输入联系人性别：" << endl;
+        cout << "1 --- 男" << endl;
+        cout << "2 --- 女" << endl;
+
+        while (true) // 判断输入是否1或2，直至正确break
+        {
+            cin >> gender;
+            if (gender == 1 || gender == 2)
+            {
+                abs->personArray[ret].m_Gender = gender;
+                break;
+            }
+            cout << "请输入有误请重试" << endl;
+        }
+
+        // 输入年龄
+        int age = 0;
+        cout << "请输入联系人年龄：" << endl;
+        cin >> age;
+        abs->personArray[ret].m_Age = age;
+
+        // 输入电话
+        string phone;
+        cout << "请输入联系人电话：" << endl;
+        cin >> phone;
+        abs->personArray[ret].m_Phone = phone;
+
+        // 输入地址
+        string address;
+        cout << "请输入联系人地址：" << endl;
+        cin >> address;
+        abs->personArray[ret].m_Address = address;
+
+        cout << "修改成功" << endl;
+
+    } else {    // 不存在
+        cout << "没有找到该联系人" << endl;
+    }
+
+    system("pause");
+    system("cls");  //清屏
+}
+
+// 清空通讯录
+void cleanPerson(AddressBooks *abs)
+{
+    int isClean = 0;
+    cout << "是否确认清空通讯录？" << endl;
+    cout << "1 --- 确认" << endl;
+    cout << "2 --- 取消" << endl;
+    cin >> isClean;
+
+    if (isClean == 1)
+    {
+        abs->m_Size = 0;
+        cout << "清空成功" << endl;
+    } else {
+        cout << "取消清空" << endl;
+    }
+
+    system("pause");
+    system("cls");  //清屏
+}
 
 // 菜单界面
 void showMenu()
@@ -167,33 +299,20 @@ int main()
             showPerson(&abs);
             break;
         case 3:
-        {   // 如果 switch 内代码过程可能会报错，此时可以使用{}把代码括起来
             /* 删除联系人 */
-            cout << "请输入要删除的联系人姓名：" << endl;
-            string name;
-            cin >> name;
-            // 判断联系人是否存在
-            if (isExist(&abs, name) == -1)
-            {
-                cout << "联系人不存在，无法删除" << endl;
-            }
-            else
-            {
-                // 删除联系人
-                // deletePerson(&abs, name);
-
-                cout << "删除成功" << endl;
-            }
-        }
+            deletePerson(&abs);
             break;
         case 4:
             /* 查询联系人 */
+            findPseson(&abs);
             break;
         case 5:
             /* 修改联系人 */
+            modifyPerson(&abs);
             break;
         case 6:
             /* 清空联系人 */
+            cleanPerson(&abs);
             break;
         case 0:
             /* 退出系统 */
